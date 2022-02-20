dd7ec41018994226f16dba7ce9d97d569db6327a
kathrine-mcaulay <kathrine.mcaulay@gmail.com>
2022-02-20 11:01:25 -0500

    Final (hopefully) fix for setting up git/Rstudio on new laptop

--- test.R
+++ test.R
@@ -1,3 +1,7 @@
 print("This file was created within RStudio")
 
 print("And now it lives on GitHub")
+
+print("Some additional problems in Feb 2022")
+
+print("But it looks like everything works now")
